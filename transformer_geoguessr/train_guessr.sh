bdd26d64416ec9f52401a9e323808ceefdfe99b4
Sun <suny4@mski3240.mskcc.root.mskcc.org>
2023-10-31 17:50:12 -0400

    correct path for train

--- transformer_geoguessr/train_guessr.sh
+++ transformer_geoguessr/train_guessr.sh
@@ -7,7 +7,7 @@
 
 conda activate cuda111_torch
 
-python cnn_geoguessr/train.py --root_dir /data/leslie/suny4/geo/world_panorama/ \
+python train.py --root_dir /data/leslie/suny4/geo/world_panorama/ \
     --epochs 150 --save_model ./results/ --plot_loss ./results/ --plot_map ./results/
 
 conda deactivate
