cedfa08a0d7d914cd57694fcb2ff838d90917d26
Yuchen Sun <kevin.sun0612@hotmail.com>
2023-11-28 13:13:35 -0500

    separate running sh for baseline and vgg16

---
+++ cnn_geoguessr/train_guessr_baseline.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -l
+
+#BSUB -J cnnguessr
+#BSUB -oo hpc_logs/cnnguessr_baseline.out
+#BSUB -eo hpc_logs/cnnguessr_baseline.err
+
+model="baseline"
+epoch=100
+optim="Adam"
+lr="1e-3"
+batch=32
+
+conda activate cuda111_torch
+
+python train.py --root_dir /data/leslie/suny4/geo/world_panorama/ \
+    --model $model --epochs $epoch --optimizer $optim --lr $lr --batch_size $batch \
+    --out_dir "./results/${model}_b${batch}_${optim}${lr}_${epoch}/" \
+    --save_model
+
+conda deactivate
--- cnn_geoguessr/train_guessr_vgg16.sh
+++ cnn_geoguessr/train_guessr_vgg16.sh
@@ -1,19 +1,19 @@
 #!/bin/bash -l
 
 #BSUB -J cnnguessr
-#BSUB -oo hpc_logs/cnnguessr.out
-#BSUB -eo hpc_logs/cnnguessr.err
+#BSUB -oo hpc_logs/cnnguessr_vgg16.out
+#BSUB -eo hpc_logs/cnnguessr_vgg16.err
 
-model="baseline"
-epoch=100
+model="vgg16"
+epoch=200
 optim="Adam"
-lr="1e-3"
-batch=32
+lr="5e-4"
+batch=16
 
 conda activate cuda111_torch
 
 python train.py --root_dir /data/leslie/suny4/geo/world_panorama/ \
-    --model $model --epochs $epoch --optimizer $optim --lr $lr --batch_size $batch \
+    --model $model --augment --epochs $epoch --optimizer $optim --lr $lr --batch_size $batch \
     --out_dir "./results/${model}_b${batch}_${optim}${lr}_${epoch}/" \
     --save_model
 
