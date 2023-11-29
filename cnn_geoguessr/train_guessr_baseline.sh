0759a182b1579edb94b64b466953d81711647bae
Yuchen Sun <kevin.sun0612@hotmail.com>
2023-11-29 00:57:58 -0500

    add augment to basline

--- cnn_geoguessr/train_guessr_baseline.sh
+++ cnn_geoguessr/train_guessr_baseline.sh
@@ -5,16 +5,17 @@
 #BSUB -eo hpc_logs/cnnguessr_baseline.err
 
 model="baseline"
-epoch=100
+epoch=250
 optim="Adam"
-lr="1e-3"
-batch=32
+lr="5e-4"
+batch=16
+augment="aug"
 
 conda activate cuda111_torch
 
 python train.py --root_dir /data/leslie/suny4/geo/world_panorama/ \
-    --model $model --epochs $epoch --optimizer $optim --lr $lr --batch_size $batch \
-    --out_dir "./results/${model}_b${batch}_${optim}${lr}_${epoch}/" \
+    --model $model --augment --epochs $epoch --optimizer $optim --lr $lr --batch_size $batch \
+    --out_dir "./results/${model}_${augment}_b${batch}_${optim}${lr}_${epoch}/" \
     --save_model
 
 conda deactivate
